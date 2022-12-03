a9aa5b03763a99241d58e097dd4e7801220257d9
fulltime117 <fulltime117@gmail.com>
2022-12-03 21:49:25 +0800

    complete project

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const colors = require('colors')
 require('dotenv').config()
 const { graphqlHTTP } = require('express-graphql')
@@ -10,13 +11,14 @@ const app = express()
 
 //Connect to database
 connectDB();
+app.use(cors())
 
 app.use(
     '/qraphql',
     graphqlHTTP({
         schema,
         graphiql: process.env.NODE_ENV === 'development'
-    })
+    }),
 )
 
 app.listen(port, console.log(`Server running on port ${port}`))
